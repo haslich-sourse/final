a8ebe65eb37097e50e1712dc33099fef2c65ebb0
haslich-sourse <igor.mizev01@gmail.com>
2024-10-18 20:33:55 +0500

    js

--- magic_frontend/magic.js
+++ magic_frontend/magic.js
@@ -5,3 +5,18 @@ const menu = document.querySelector('.menu');
 burgerMenu.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+// Плавное появление слов с задержкой
+const texts = document.querySelectorAll('.geometric-layout p');
+
+texts.forEach((text, index) => {
+    const words = text.innerHTML.split(' ');
+    text.innerHTML = ''; // Очищаем текст
+
+    words.forEach((word, i) => {
+        setTimeout(() => {
+            text.innerHTML += word + ' ';
+        }, i * 150); // Появление каждого слова с задержкой
+    });
+});
+
+// Превращение круга в квадрат и наоборот при клике
