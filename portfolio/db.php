5fc6eb0572e681a0f925ca0d4d9c4070b76a8b56
haslich-sourse <igor.mizev01@gmail.com>
2024-10-23 15:23:29 +0500

    ffsd

--- portfolio/db.php
+++ portfolio/db.php
@@ -4,7 +4,7 @@
 $host = 'localhost';    // хост базы данных
 $db = 'form_data';      // имя базы данных
 $user = 'root';         // имя пользователя
-$pass = '135phpmyadmin315';             // пароль
+$pass = '';             // пароль
 
 try {
     // Создаем объект PDO
